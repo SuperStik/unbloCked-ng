197076aba25ee3f0190e27c923e6c6ed1becdded
SuperStik <daviswood290@gmail.com>
2026-02-22 20:21:17 -0800

    Disable unnecessary modules in miniaudio

--- src/sound/sound.h
+++ src/sound/sound.h
@@ -1,6 +1,10 @@
 #ifndef UBLC_SOUND
 #define UBLC_SOUND 1
 
+#define MA_NO_ENCODING
+#define MA_NO_FLAC
+#define MA_NO_MP3
+#define MA_NO_WAV
 #include <sound/miniaudio.h>
 
 extern ma_engine engine;
