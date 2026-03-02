80b3b4f32d92fbd5bb65da6b156662a1de74883b
SuperStik <daviswood290@gmail.com>
2026-03-02 10:57:04 -0800

    Fix nastiness with paletted PNGs

--- src/image/png.c
+++ src/image/png.c
@@ -26,23 +26,12 @@ unsigned char *img_readpng(FILE *file, uint32_t *width, uint32_t *height, int *
 		return NULL;
 	}
 
-	png_set_palette_to_rgb(png_reader);
-
-	png_init_io(png_reader, file);
-	png_set_sig_bytes(png_reader, 8);
-
 	png_infop png_info = png_create_info_struct(png_reader);
 	if (png_info == NULL) {
 		warnx("libpng: Can't create info");
 		png_destroy_read_struct(&png_reader, NULL, NULL);
 		return NULL;
 	}
-
-	png_read_info(png_reader, png_info);
-
-#if __BYTE_ORDER__ != __ORDER_BIG_ENDIAN__
-	png_set_swap(png_reader);
-#endif /* __ORDER_BIG_ENDIAN__ */
 
 	if (setjmp(png_jmpbuf(png_reader))) {
 		warnx("libpng: Failed to read png");
@@ -51,6 +40,19 @@ unsigned char *img_readpng(FILE *file, uint32_t *width, uint32_t *height, int *
 		return NULL;
 	}
 
+	png_init_io(png_reader, file);
+	png_set_sig_bytes(png_reader, 8);
+
+	png_set_expand(png_reader);
+
+	png_read_info(png_reader, png_info);
+
+#if __BYTE_ORDER__ != __ORDER_BIG_ENDIAN__
+	png_set_swap(png_reader);
+#endif /* __BYTE_ORDER__ != __ORDER_BIG_ENDIAN__ */
+
+	png_read_update_info(png_reader, png_info);
+
 	int color_type;
 	png_uint_32 w, h;
 	png_get_IHDR(png_reader, png_info, &w, &h, bit_depth,
@@ -58,25 +60,9 @@ unsigned char *img_readpng(FILE *file, uint32_t *width, uint32_t *height, int *
 	*width = w;
 	*height = h;
 
-	*channels = typepng2chan(color_type);
+	*channels = png_get_channels(png_reader, png_info);
 
 	*rowbytes = png_get_rowbytes(png_reader, png_info);
-
-	/* hack to support paletted PNGs with alpha channel */
-	if (color_type == PNG_COLOR_TYPE_PALETTE) {
-		png_bytep trans = NULL;
-		int num_trans = 0;
-		png_color_16p trans_values = NULL;
-
-		/* not sure if these need to be freed somewhere */
-		png_get_tRNS(png_reader, png_info, &trans, &num_trans,
-				&trans_values);
-		if (trans != NULL) {
-			*rowbytes *= 4;
-			*channels = 4;
-		} else
-			*rowbytes *= 3;
-	}
 
 	unsigned char *image = (unsigned char *)malloc((*height) *
 			sizeof(png_byte) * *rowbytes);
@@ -106,29 +92,4 @@ unsigned char *img_readpng(FILE *file, uint32_t *width, uint32_t *height, int *
 	png_destroy_read_struct(&png_reader, &png_info, NULL);
 
 	return image;
-}
-
-static int typepng2chan(int color_type) {
-	int channels;
-	switch(color_type) {
-		case PNG_COLOR_TYPE_GRAY:
-			channels = 1;
-			break;
-		case PNG_COLOR_TYPE_GRAY_ALPHA:
-			channels = 2;
-			break;
-		case PNG_COLOR_TYPE_PALETTE:
-		case PNG_COLOR_TYPE_RGB:
-			channels = 3;
-			break;
-		case PNG_COLOR_TYPE_RGB_ALPHA:
-			channels = 4;
-			break;
-		default:
-			warnx("pngreader: bad color type: %i",
-					color_type);
-			channels = -1;
-	}
-
-	return channels;
 }
