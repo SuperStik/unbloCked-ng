ea7408d917ee5fb6448036593f8b6b61adb03f49
SuperStik <daviswood290@gmail.com>
2026-03-22 13:07:40 -0700

    Fix regression with cursor not storing previous information

--- src/shared/cursor.c
+++ src/shared/cursor.c
@@ -5,6 +5,11 @@
 static SDL_Cursor *cursor;
 
 int cursor_set(SDL_SystemCursor id) {
+	static SDL_SystemCursor current_id = SDL_SYSTEM_CURSOR_DEFAULT;
+	if (__builtin_expect(current_id == id, 1))
+		return 1;
+	current_id = id;
+
 	cursor_free();
 
 	cursor = SDL_CreateSystemCursor(id);
