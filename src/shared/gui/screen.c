fcf086a9077511584f8e9196ce4c57c20844f476
SuperStik <daviswood290@gmail.com>
2026-03-22 17:07:26 -0700

    Add mutex to screen

--- src/shared/gui/screen.c
+++ src/shared/gui/screen.c
@@ -6,6 +6,9 @@ struct gui_screen *gui_screen_init(struct gui_screen *screen, float w, float h,
 	screen->width = w;
 	screen->height = h;
 
+	pthread_mutex_init(&screen->mutex, NULL);
+	gui_screen_lock(screen);
+
 	switch(type) {
 		case GUI_SCREEN_MAINMENU:
 			gui_mainmenu_init(&screen->screens.mainmenu);
@@ -14,10 +17,14 @@ struct gui_screen *gui_screen_init(struct gui_screen *screen, float w, float h,
 			break;
 	}
 
+	gui_screen_unlock(screen);
+
 	return screen;
 }
 
 void gui_screen_destroy(struct gui_screen *screen) {
+	gui_screen_lock(screen);
+
 	switch(screen->type) {
 		case GUI_SCREEN_MAINMENU:
 			gui_mainmenu_destroy(&screen->screens.mainmenu);
@@ -25,14 +32,23 @@ void gui_screen_destroy(struct gui_screen *screen) {
 		case GUI_SCREEN_MAX:
 			break;
 	}
+
+	gui_screen_unlock(screen);
+	pthread_mutex_destroy(&screen->mutex);
 }
 
 void gui_screen_resize(struct gui_screen *screen, float w, float h) {
+	gui_screen_lock(screen);
+
 	screen->width = w;
 	screen->height = h;
+
+	gui_screen_unlock(screen);
 }
 
 void gui_screen_onclick(struct gui_screen *screen) {
+	gui_screen_lock(screen);
+
 	switch(screen->type) {
 		case GUI_SCREEN_MAINMENU:
 			gui_mainmenu_onclick(&screen->screens.mainmenu);
@@ -40,11 +56,16 @@ void gui_screen_onclick(struct gui_screen *screen) {
 		case GUI_SCREEN_MAX:
 			break;
 	}
+
+	gui_screen_unlock(screen);
 }
 
 void gui_screen_onhover(struct gui_screen *screen, float x, float y) {
 	gvec(float,2) pos = {x, y};
 	gvec(float,2) area = {screen->width, screen->height};
+
+	gui_screen_lock(screen);
+
 	switch(screen->type) {
 		case GUI_SCREEN_MAINMENU:
 			gui_mainmenu_onhover(&screen->screens.mainmenu, pos,
@@ -53,4 +74,14 @@ void gui_screen_onhover(struct gui_screen *screen, float x, float y) {
 		case GUI_SCREEN_MAX:
 			break;
 	}
+
+	gui_screen_unlock(screen);
+}
+
+int gui_screen_lock(struct gui_screen *screen) {
+	return pthread_mutex_lock(&screen->mutex);
+}
+
+int gui_screen_unlock(struct gui_screen *screen) {
+	return pthread_mutex_unlock(&screen->mutex);
 }
--- src/shared/gui/screen.h
+++ src/shared/gui/screen.h
@@ -1,6 +1,7 @@
 #ifndef GUI_SCREEN_H
 #define GUI_SCREEN_H 1
 
+#include <pthread.h>
 #include <stddef.h>
 #include <stdint.h>
 
@@ -12,6 +13,7 @@ enum gui_screen_type {
 };
 
 struct gui_screen {
+	pthread_mutex_t mutex;
 	enum gui_screen_type type;
 	union {
 		struct gui_mainmenu mainmenu;
@@ -30,5 +32,8 @@ void gui_screen_resize(struct gui_screen *, float width, float height);
 void gui_screen_onclick(struct gui_screen *);
 
 void gui_screen_onhover(struct gui_screen *, float x, float y);
+
+int gui_screen_lock(struct gui_screen *);
+int gui_screen_unlock(struct gui_screen *);
 
 #endif /* GUI_SCREEN_H */
