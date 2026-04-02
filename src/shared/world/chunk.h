4f35165f7064c6362dec281b26c4e7e1a6ace1f4
SuperStik <daviswood290@gmail.com>
2026-04-02 14:24:51 -0700

    Add chunk and world structures

---
+++ src/shared/world/chunk.h
@@ -0,0 +1,23 @@
+#ifndef WORLD_CHUNK_H
+#define WORLD_CHUNK_H 1
+
+#define CHUNK_SIDE 16UL
+#define CHUNK_HEIGHT 128UL
+#define CHUNK_BLOCKS ((CHUNK_SIDE) * (CHUNK_SIDE) * (CHUNK_HEIGHT))
+
+#include <world/world.h>
+
+struct ublc_chunk {
+	struct ublc_world *world;
+	long long xpos;
+	long long zpos;
+	unsigned char blocks[CHUNK_BLOCKS];
+};
+
+void ublc_chunk_init_empty(struct ublc_chunk *restrict, struct ublc_world *
+		restrict, long long xpos, long long zpos);
+void ublc_chunk_init_blocks(struct ublc_chunk *restrict, struct ublc_world *
+		restrict, long long xpos, long long zpos, const unsigned char
+		blocks[CHUNK_BLOCKS]);
+
+#endif /* WORLD_CHUNK_H */
---
+++ src/shared/world/world.h
@@ -0,0 +1,8 @@
+#ifndef WORLD_WORLD_H
+#define WORLD_WORLD_H 1
+
+struct ublc_world {
+	struct ublc_chunk *chunks;
+};
+
+#endif /* WORLD_WORLD_H */
