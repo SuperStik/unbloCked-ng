6ca331a5193b5881655cd3f48086c2b8bd6d8e9f
SuperStik <daviswood290@gmail.com>
2026-04-08 13:32:28 -0700

    Add get function to chunklist header

--- src/shared/world/chunklist.h
+++ src/shared/world/chunklist.h
@@ -18,6 +18,9 @@ struct ublc_chunklist *ublc_chunklist_init(struct ublc_chunklist *, size_t
 		start_size);
 void ublc_chunklist_delete(const struct ublc_chunklist *);
 
+struct ublc_chunk *ublc_chunklist_get(const struct ublc_chunklist *, long long
+		x, long long z);
+
 struct ublc_chunklist *ublc_chunklist_insert(struct ublc_chunklist *, const
 		struct ublc_chunk *);
 struct ublc_chunklist *ublc_chunklist_remove(struct ublc_chunklist *, long long
